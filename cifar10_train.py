a441dac9046c148f19a0e46cd4a197be7cf46b09
Pam Johnston <pam@RG45609.rgu.ac.uk>
2017-05-25 10:33:39 +0100

    import path name

--- cifar10_train.py
+++ cifar10_train.py
@@ -93,7 +93,7 @@ def train():
     # Start the queue runners.
     tf.train.start_queue_runners(sess=sess)
 
-    summary_writer = tf.train.SummaryWriter(FLAGS.train_dir, sess.graph)
+    summary_writer = tf.summary.FileWriter(FLAGS.train_dir, sess.graph)
 
     for step in xrange(FLAGS.max_steps):
       start_time = time.time()
