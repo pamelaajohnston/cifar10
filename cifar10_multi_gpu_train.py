0a4b4069e0e9d448f5c6f6007a3b1ab078c2386a
Pam Johnston <pam@RG45609.rgu.ac.uk>
2017-05-25 11:19:50 +0100

    tensorflow reuse variables stuff

--- cifar10_multi_gpu_train.py
+++ cifar10_multi_gpu_train.py
@@ -147,31 +147,32 @@ def average_gradients(tower_grads):
 
 def train():
   """Train CIFAR-10 for a number of steps."""
-  with tf.variable_scope(tf.get_variable_scope()) as vscope:
-      with tf.Graph().as_default(), tf.device('/cpu:0'):
-        # Create a variable to count the number of train() calls. This equals the
-        # number of batches processed * FLAGS.num_gpus.
-        global_step = tf.get_variable(
-            'global_step', [],
-            initializer=tf.constant_initializer(0), trainable=False)
+  with tf.Graph().as_default(), tf.device('/cpu:0'):
+    # Create a variable to count the number of train() calls. This equals the
+    # number of batches processed * FLAGS.num_gpus.
+    global_step = tf.get_variable(
+        'global_step', [],
+        initializer=tf.constant_initializer(0), trainable=False)
 
-        # Calculate the learning rate schedule.
-        num_batches_per_epoch = (cifar10.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN /
-                                 FLAGS.batch_size)
-        decay_steps = int(num_batches_per_epoch * cifar10.NUM_EPOCHS_PER_DECAY)
+    # Calculate the learning rate schedule.
+    num_batches_per_epoch = (cifar10.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN /
+                             FLAGS.batch_size)
+    decay_steps = int(num_batches_per_epoch * cifar10.NUM_EPOCHS_PER_DECAY)
 
-        # Decay the learning rate exponentially based on the number of steps.
-        lr = tf.train.exponential_decay(cifar10.INITIAL_LEARNING_RATE,
-                                        global_step,
-                                        decay_steps,
-                                        cifar10.LEARNING_RATE_DECAY_FACTOR,
-                                        staircase=True)
+    # Decay the learning rate exponentially based on the number of steps.
+    lr = tf.train.exponential_decay(cifar10.INITIAL_LEARNING_RATE,
+                                    global_step,
+                                    decay_steps,
+                                    cifar10.LEARNING_RATE_DECAY_FACTOR,
+                                    staircase=True)
 
-        # Create an optimizer that performs gradient descent.
-        opt = tf.train.GradientDescentOptimizer(lr)
+    # Create an optimizer that performs gradient descent.
+    opt = tf.train.GradientDescentOptimizer(lr)
 
-        # Calculate the gradients for each model tower.
-        tower_grads = []
+    # Calculate the gradients for each model tower.
+    tower_grads = []
+    
+    with tf.variable_scope(tf.get_variable_scope()) as scope:
         for i in xrange(FLAGS.num_gpus):
           with tf.device('/gpu:%d' % i):
             with tf.name_scope('%s_%d' % (cifar10.TOWER_NAME, i)) as scope:
@@ -192,81 +193,81 @@ def train():
               # Keep track of the gradients across all towers.
               tower_grads.append(grads)
 
-        # We must calculate the mean of each gradient. Note that this is the
-        # synchronization point across all towers.
-        grads = average_gradients(tower_grads)
+    # We must calculate the mean of each gradient. Note that this is the
+    # synchronization point across all towers.
+    grads = average_gradients(tower_grads)
 
-        # Add a summary to track the learning rate.
-        summaries.append(tf.summary.scalar('learning_rate', lr))
+    # Add a summary to track the learning rate.
+    summaries.append(tf.summary.scalar('learning_rate', lr))
 
-        # Add histograms for gradients.
-        for grad, var in grads:
-          if grad is not None:
-            summaries.append(
-                tf.summary.histogram(var.op.name + '/gradients', grad))
+    # Add histograms for gradients.
+    for grad, var in grads:
+      if grad is not None:
+        summaries.append(
+            tf.summary.histogram(var.op.name + '/gradients', grad))
 
-        # Apply the gradients to adjust the shared variables.
-        apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
+    # Apply the gradients to adjust the shared variables.
+    apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
 
-        # Add histograms for trainable variables.
-        for var in tf.trainable_variables():
-          summaries.append(tf.summary.histogram(var.op.name, var))
+    # Add histograms for trainable variables.
+    for var in tf.trainable_variables():
+      summaries.append(tf.summary.histogram(var.op.name, var))
 
-        # Track the moving averages of all trainable variables.
-        variable_averages = tf.train.ExponentialMovingAverage(
-            cifar10.MOVING_AVERAGE_DECAY, global_step)
-        variables_averages_op = variable_averages.apply(tf.trainable_variables())
+    # Track the moving averages of all trainable variables.
+    variable_averages = tf.train.ExponentialMovingAverage(
+        cifar10.MOVING_AVERAGE_DECAY, global_step)
+    variables_averages_op = variable_averages.apply(tf.trainable_variables())
 
-        # Group all updates to into a single train op.
-        train_op = tf.group(apply_gradient_op, variables_averages_op)
+    # Group all updates to into a single train op.
+    train_op = tf.group(apply_gradient_op, variables_averages_op)
 
-        # Create a saver.
-        saver = tf.train.Saver(tf.all_variables())
+    # Create a saver.
+    saver = tf.train.Saver(tf.all_variables())
 
-        # Build the summary operation from the last tower summaries.
-        summary_op = tf.summary.merge(summaries)
+    # Build the summary operation from the last tower summaries.
+    summary_op = tf.summary.merge(summaries)
 
-        # Build an initialization operation to run below.
-        init = tf.initialize_all_variables()
+    # Build an initialization operation to run below.
+    init = tf.initialize_all_variables()
 
-        # Start running operations on the Graph. allow_soft_placement must be set to
-        # True to build towers on GPU, as some of the ops do not have GPU
-        # implementations.
-        sess = tf.Session(config=tf.ConfigProto(
-            allow_soft_placement=True,
-            log_device_placement=FLAGS.log_device_placement))
-        sess.run(init)
+    # Start running operations on the Graph. allow_soft_placement must be set to
+    # True to build towers on GPU, as some of the ops do not have GPU
+    # implementations.
+    sess = tf.Session(config=tf.ConfigProto(
+        allow_soft_placement=True,
+        log_device_placement=FLAGS.log_device_placement))
+    sess.run(init)
 
-        # Start the queue runners.
-        tf.train.start_queue_runners(sess=sess)
+    # Start the queue runners.
+    tf.train.start_queue_runners(sess=sess)
 
-        summary_writer = tf.summary.FileWriter(FLAGS.train_dir, sess.graph)
+    summary_writer = tf.summary.FileWriter(FLAGS.train_dir, sess.graph)
 
-        for step in xrange(FLAGS.max_steps):
-          start_time = time.time()
-          _, loss_value = sess.run([train_op, loss])
-          duration = time.time() - start_time
+    for step in xrange(FLAGS.max_steps):
+      start_time = time.time()
+      _, loss_value = sess.run([train_op, loss])
+      duration = time.time() - start_time
 
-          assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+      assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
 
-          if step % 10 == 0:
-            num_examples_per_step = FLAGS.batch_size * FLAGS.num_gpus
-            examples_per_sec = num_examples_per_step / duration
-            sec_per_batch = duration / FLAGS.num_gpus
+      if step % 10 == 0:
+        num_examples_per_step = FLAGS.batch_size * FLAGS.num_gpus
+        examples_per_sec = num_examples_per_step / duration
+        sec_per_batch = duration / FLAGS.num_gpus
 
-            format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
-                          'sec/batch)')
-            print (format_str % (datetime.now(), step, loss_value,
-                                 examples_per_sec, sec_per_batch))
+        format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
+                      'sec/batch)')
+        print (format_str % (datetime.now(), step, loss_value,
+                             examples_per_sec, sec_per_batch))
 
-          if step % 100 == 0:
-            summary_str = sess.run(summary_op)
-            summary_writer.add_summary(summary_str, step)
+      if step % 100 == 0:
+        summary_str = sess.run(summary_op)
+        summary_writer.add_summary(summary_str, step)
 
-          # Save the model checkpoint periodically.
-          if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
-            checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
-            saver.save(sess, checkpoint_path, global_step=step)
+      # Save the model checkpoint periodically.
+      if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
+        checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
+        saver.save(sess, checkpoint_path, global_step=step)
 
 
 def main(argv=None):  # pylint: disable=unused-argument
