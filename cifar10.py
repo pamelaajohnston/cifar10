c3621fd50c3246d52ff814833bccde0f0818b546
Pam Johnston <1609098@rgu.ac.uk>
2017-08-08 10:08:00 +0100

    adjust the learning rate stuff again

--- cifar10.py
+++ cifar10.py
@@ -60,7 +60,7 @@ MOVING_AVERAGE_DECAY = 0.9999     # The decay to use for the moving average.
 #NUM_EPOCHS_PER_DECAY = 350.0      # Epochs after which learning rate decays.
 NUM_EPOCHS_PER_DECAY = 1750.0      # Epochs after which learning rate decays.
 LEARNING_RATE_DECAY_FACTOR = 0.1  # Learning rate decay factor.
-INITIAL_LEARNING_RATE = 0.01       # Initial learning rate.
+INITIAL_LEARNING_RATE = 0.1       # Initial learning rate.
 
 # If a model is trained with multiple GPUs, prefix all Op names with tower_name
 # to differentiate the operations. Note that this prefix is removed from the
@@ -964,6 +964,7 @@ def train(total_loss, global_step):
   # Variables that affect learning rate.
   num_batches_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN / FLAGS.batch_size
   decay_steps = int(num_batches_per_epoch * NUM_EPOCHS_PER_DECAY)
+  decay_steps = 2000
 
   # Decay the learning rate exponentially based on the number of steps.
   lr = tf.train.exponential_decay(INITIAL_LEARNING_RATE,
