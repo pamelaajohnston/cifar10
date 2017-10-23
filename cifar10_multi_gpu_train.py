805d9697c62cd6e3fd4a8436cf2840faf284a77c
Pam Johnston <1609098@rgu.ac.uk>
2017-10-23 16:52:39 +0100

    stupid stupid

--- cifar10_multi_gpu_train.py
+++ cifar10_multi_gpu_train.py
@@ -444,7 +444,7 @@ def main_justTheOne(argv=None):  # pylint: disable=unused-argument
     saveFrames = (0, 2, 3, 6)
     saveFrames = (0,)
     quants = (10, 25, 37, 41, 46, 50)
-    quants = (10, 25, 37, 50)
+    #quants = (10, 25, 37, 50)
     #myDatadirs = ["yuv", "nn", "y_quv", "y_squv", "interlaced"]
     myDatadirs = ["yuv", "y_quv", "y_squv", "interlaced"]
     myDatadirs = ["yuv", "all"]
