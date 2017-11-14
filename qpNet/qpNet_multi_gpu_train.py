b689ff08c02e3e81df392031e25a3e542d2806c1
Pam Johnston <1609098@rgu.ac.uk>
2017-11-14 11:45:37 +0000

    Added stdout flushing because it was kind of quiet

--- qpNet/qpNet_multi_gpu_train.py
+++ qpNet/qpNet_multi_gpu_train.py
@@ -35,6 +35,7 @@ from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 import qpNet
 import qpNet_eval
+import sys
 #import image2vid
 
 FLAGS = tf.app.flags.FLAGS
@@ -250,6 +251,7 @@ def train():
 
         format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f ' 'sec/batch)')
         print (format_str % (datetime.now(), step, loss_value, examples_per_sec, sec_per_batch))
+        sys.stdout.flush()
 
       if step % 100 == 0:
         summary_str = sess.run(summary_op)
